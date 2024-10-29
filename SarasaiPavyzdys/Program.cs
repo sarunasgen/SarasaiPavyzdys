7b6c14f86a6d13692e5ec36d9f663b4bd5251d55
Sarunas <sarugen12@gmail.com>
2024-10-29 09:22:28 +0200

    Add project files.

---
+++ SarasaiPavyzdys/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SarasaiPavyzdys
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> sveikiejiSkaiciai = new List<int>();
+            Console.WriteLine("Kiek skaiciu noresite ivesti: ");
+            int kiekis = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < kiekis; i++)
+            {
+                Console.WriteLine($"Iveskite {i+1} skaiciu: ");
+                sveikiejiSkaiciai.Add(int.Parse(Console.ReadLine()));
+            }
+            List<int> kitiSveikiejiSkaiciai = new List<int>();
+            kitiSveikiejiSkaiciai.Add(5);
+            kitiSveikiejiSkaiciai.Add(8);
+            kitiSveikiejiSkaiciai.Add(19);
+
+
+            sveikiejiSkaiciai.AddRange(kitiSveikiejiSkaiciai);
+            sveikiejiSkaiciai.Remove(8);
+            sveikiejiSkaiciai.Insert(1, -99);
+            if(sveikiejiSkaiciai.Contains(-128))
+            {
+                Console.WriteLine("-128 yra sarase");
+            }
+            else
+            {
+                Console.WriteLine("-128 nera sarase");
+            }
+            
+            int index = sveikiejiSkaiciai.IndexOf(19);
+
+            foreach(int skaicius in sveikiejiSkaiciai)
+            {
+                Console.WriteLine(skaicius);
+            }
+
+
+        }
+    }
+}
